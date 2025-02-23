77848c39b3c87d537ae32c7209bdf1524e8ec335
Marat <80181316+MaratFa@users.noreply.github.com>
2025-02-23 22:27:26 +0300

    Solution

---
+++ 1-objects-task-4.js
@@ -0,0 +1,19 @@
+/* 
+  Multiply numeric property values by 2
+*/
+  
+  let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+  };
+
+  function multiplyNumeric(menu) {
+    for (let prop in menu) {
+      if (typeof menu[prop] === "number") menu[prop] *= 2;
+    }
+  };
+  
+  multiplyNumeric(menu);
+  
+  console.log(menu);
