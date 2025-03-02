4dec75d3ed4910d25c2c760dd9ee3e92c8cdee4e
Marat <80181316+MaratFa@users.noreply.github.com>
2025-03-02 22:18:39 +0300

    Update 0-objects-multiply-numeric-property-values-by-2.js

--- 0-objects-multiply-numeric-property-values-by-2.js
+++ 0-objects-multiply-numeric-property-values-by-2.js
@@ -1,15 +1,15 @@
-  let menu = {
-    width: 200,
-    height: 300,
-    title: "My menu"
-  };
+let menu = {
+  width: 200,
+  height: 300,
+  title: "My menu"
+};
 
-  function multiplyNumeric(menu) {
-    for (let prop in menu) {
-      if (typeof menu[prop] === "number") menu[prop] *= 2;
-    }
-  };
+function multiplyNumeric(menu) {
+  for (let prop in menu) {
+    if (typeof menu[prop] === "number") menu[prop] *= 2;
+  }
+};
   
-  multiplyNumeric(menu);
+multiplyNumeric(menu);
   
-  console.log(menu);
+console.log(menu);
