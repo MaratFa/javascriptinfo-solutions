dcc620c6cb0a02630750ad50aafb882ac49312ad
Marat <80181316+MaratFa@users.noreply.github.com>
2025-03-23 02:25:02 +0300

    Add files via upload

---
+++ 7-data-types_numbers_1.js
@@ -0,0 +1,11 @@
+/*
+Sum numbers from the visitor. 
+
+Create a script that prompts the visitor
+to enter two numbers and then shows their sum.
+*/
+
+let a = +prompt("Enter the first number");
+let b = +prompt("Enter the second number");
+
+alert(`The sum is ${a + b}`);
