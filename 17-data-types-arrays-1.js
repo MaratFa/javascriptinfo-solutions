fd089714e56d5d64a02cae18834662009e20c3bb
Marat <80181316+MaratFa@users.noreply.github.com>
2025-04-12 23:38:44 +0300

    Add files via upload

---
+++ 17-data-types-arrays-1.js
@@ -0,0 +1,13 @@
+/*
+Is array copied?
+
+What is this code going to show?
+
+let fruits = ["Apples", "Pear", "Orange"];
+let shoppingCart = fruits;
+shoppingCart.push("Banana");
+
+alert( fruits.length );
+*/
+
+4
